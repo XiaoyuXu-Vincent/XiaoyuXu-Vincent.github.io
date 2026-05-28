7ed93d003b3f0f961eb25bc43ba955169d1a1928
x13onfire <10476753@qq.com>
2026-05-28 20:48:54 +0800

    Redesign: adopt AcadHomepage-style academic template

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
-/* Minimal JS for academic homepage */
 (function() {
     'use strict';
-
     document.querySelectorAll('a[href^="#"]').forEach(function(anchor) {
         anchor.addEventListener('click', function(e) {
             var href = this.getAttribute('href');
